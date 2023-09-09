43a923c2fbb94ade14135605941a42d56792e29d
Brian Skinn <brian.skinn@gmail.com>
2023-09-09 16:33:33 -0500

    Refactor to functions and add get for move info

--- scrape.js
+++ scrape.js
@@ -1,16 +1,39 @@
-const report = () => {
-
 var ids = []
 $$('div[class="player-name"]').forEach(s => ids.push(s.id.split("_")[2]))
 
-var names = []
-ids.forEach(s => names.push($$(`div[id$="${s}"][class="player-name"]`)[0].textContent.trim()))
+const getNames = () => {
+  var names = []
 
-var scores = []
-ids.forEach(s => scores.push($$(`span[id$="${s}"][class^="player_score"]`)[0].textContent.trim()))
+  ids.forEach(s => names.push($$(`div[id$="${s}"][class="player-name"]`)[0].textContent.trim()))
 
-console.log(`${names.join()}\n${scores.join()}`)
+  return names
+}
+
+const getScores = () => {
+  var scores = []
+
+  ids.forEach(s => scores.push($$(`span[id$="${s}"][class^="player_score"]`)[0].textContent.trim()))
+
+  return scores
+}
+
+const getMoveInfo = () => {
+  var moveInfo = []
+
+  $$('div[id^="replaylogs_move_"]').forEach(s => moveInfo.push(s.textContent.match(/Move (\d+)\s*[0-9:]+\s*[AP]M(.{30})/)))
+
+  return moveInfo
+}
+
+const getNamedMoves = (moveInfo) => {
+  const names = getNames()
+
 
 }
 
+const report = () => {
+  const names = getNames()
+  const scores = getScores()
 
+  console.log(`${names.join()}\n${scores.join()}`)
+}
