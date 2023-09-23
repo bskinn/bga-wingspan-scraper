7dce971cd5c3f88a4117ee09a23f327c66e90cd2
Brian Skinn <brian.skinn@gmail.com>
2023-09-23 12:27:52 -0600

    Clean up funcs, remove let, add some consts

--- scrape_boards.js
+++ scrape_boards.js
@@ -1,24 +1,51 @@
+// ======  USEFUL CONSTANTS  ======
+CARD_LOCATIONS = [
+  '9',
+  '10',
+  '11',
+  '12',
+  '13',
+  '17',
+  '18',
+  '19',
+  '20',
+  '21',
+  '25',
+  '26',
+  '27',
+  '28',
+  '29',
+]
+
+// ======  HELPER FUNCTIONS  ======
+cardLocationDescription = (locId) => {
+  return {
+    9: 'Forest 1',
+    10: 'Forest 2',
+    11: 'Forest 3',
+    12: 'Forest 4',
+    13: 'Forest 5',
+    17: 'Grassland 1',
+    18: 'Grassland 2',
+    19: 'Grassland 3',
+    20: 'Grassland 4',
+    21: 'Grassland 5',
+    25: 'Wetland 1',
+    26: 'Wetland 2',
+    27: 'Wetland 3',
+    28: 'Wetland 4',
+    29: 'Wetland 5',
+  }[locId]
+}
+
 // ======  BASIC DATA RETRIEVAL FUNCTIONS  ======
 
-let getIds = () => {
-  // var ids = []
-
-  // $$('div[class="player-name"]').forEach((s) => ids.push(s.id.split('_')[2]))
-
-  // return ids
-
+getIds = () => {
   return $$('div[class="player-name"]').map((div) => div.id.split('_')[2])
 }
 
-let getNames = () => {
-  const ids = getIds()
-  var names = []
-
-  ids.forEach((s) =>
-    names.push(
-      $$(`div[id$="${s}"][class="player-name"]`)[0].textContent.trim(),
-    ),
+getNames = () => {
+  return getIds().map((id) =>
+    $$(`div[id$="${id}"][class="player-name"]`)[0].textContent.trim(),
   )
-
-  return names
 }
