14150207e001bd3ea2d6caa58c7ca9deadbad3db
Brian Skinn <brian.skinn@gmail.com>
2023-09-23 13:11:04 -0600

    Rearrange funcs into the right sections

--- scrape_boards.js
+++ scrape_boards.js
@@ -21,40 +21,27 @@ NO_BIRD_ID = -1
 // ======  HELPER FUNCTIONS  ======
 cardLocationDescription = (locId) => {
   return {
-    '9': 'Forest 1',
-    '10': 'Forest 2',
-    '11': 'Forest 3',
-    '12': 'Forest 4',
-    '13': 'Forest 5',
-    '17': 'Grassland 1',
-    '18': 'Grassland 2',
-    '19': 'Grassland 3',
-    '20': 'Grassland 4',
-    '21': 'Grassland 5',
-    '25': 'Wetland 1',
-    '26': 'Wetland 2',
-    '27': 'Wetland 3',
-    '28': 'Wetland 4',
-    '29': 'Wetland 5',
+    9: 'Forest 1',
+    10: 'Forest 2',
+    11: 'Forest 3',
+    12: 'Forest 4',
+    13: 'Forest 5',
+    17: 'Grassland 1',
+    18: 'Grassland 2',
+    19: 'Grassland 3',
+    20: 'Grassland 4',
+    21: 'Grassland 5',
+    25: 'Wetland 1',
+    26: 'Wetland 2',
+    27: 'Wetland 3',
+    28: 'Wetland 4',
+    29: 'Wetland 5',
   }[locId]
 }
 
 getXYPixelValues = (xyString) => {
   mch = xyString.match(/(-?\d+)px\s+(-?\d+)px/)
   return { x: mch[1], y: mch[2] }
-}
-
-calcBirdIndex = (div) => {
-  resizeData = getComputedStyle(div)['background-size']
-  offsetData = getComputedStyle(div)['background-position']
-
-  resizeXY = getXYPixelValues(resizeData)
-  offsetXY = getXYPixelValues(offsetData)
-
-  xOffset = Math.round((-16 * parseInt(offsetXY.x)) / parseInt(resizeXY.x))
-  yOffset = Math.round((-11 * parseInt(offsetXY.y)) / parseInt(resizeXY.y))
-
-  return xOffset + 16 * yOffset
 }
 
 // ======  BASIC DATA RETRIEVAL FUNCTIONS  ======
@@ -70,6 +57,19 @@ getNames = () => {
 }
 
 // ======  BIRD IDENTIFICATION  ======
+
+calcBirdIndex = (div) => {
+  resizeData = getComputedStyle(div)['background-size']
+  offsetData = getComputedStyle(div)['background-position']
+
+  resizeXY = getXYPixelValues(resizeData)
+  offsetXY = getXYPixelValues(offsetData)
+
+  xOffset = Math.round((-16 * parseInt(offsetXY.x)) / parseInt(resizeXY.x))
+  yOffset = Math.round((-11 * parseInt(offsetXY.y)) / parseInt(resizeXY.y))
+
+  return xOffset + 16 * yOffset
+}
 
 getBirdIndex = (player, loc) => {
   divId = `bird_img_${player}_${loc}`
