384908beecec99c3a5dd0df3a659a1f6b09a34ec
Brian Skinn <brian.skinn@gmail.com>
2023-11-09 23:25:01 -0500

    Add webpack config and adjust manifest.json

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path')
+
+module.exports = {
+  mode: 'development',
+  entry: './src/scrape_scores.ts',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  output: {
+    filename: 'wingspan_scores.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+}
